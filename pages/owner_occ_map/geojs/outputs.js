a66cf5a0e8915a0af4d36bbc44d8474547c77730
jdrew3 <32502519+jdrew3@users.noreply.github.com>
2020-04-18 17:02:42 -0400

    upload to pages folder

---
+++ pages/owner_occ_map/geojs/outputs.js
@@ -0,0 +1,14 @@
+var outputs = { 
+  "type": "FeatureCollection",
+  "features": [
+    output2010.features,
+	output2011.features,
+	output2012.features,
+	output2013.features,
+	output2014.features,
+	output2015.features,
+	output2016.features,
+	output2017.features,
+	output2018.features
+  ]
+}
